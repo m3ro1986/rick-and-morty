8c9be77908062f4e0dc446a7d2ab59c74cb43b19
m3ro1986 <m3ro_1986@outlook.com>
2023-05-12 12:59:31 -0600

    6 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import useUrl from './hooks/useUrl'
 import getRandomLocation from './utils/getRandomLocation'
@@ -6,10 +6,22 @@ import ResidentCard from './components/ResidentCard'
 import Loader from './components/Loader'
 
 function App() {
-    const [witdh, setWitdh] = useState(0)
-    const [idLocation, setIdLocation] = useState(getRandomLocation())
-    const url = `https://rickandmortyapi.com/api/location/${idLocation}`
-    const [location, error, isLoading] = useUrl(url)
+    const [perPages, setPerPages] = useState(12);
+    const [lastpage, setLastpage] = useState(12)
+    const [initialPage, setInitialPage] = useState(0)
+    const [witdh, setWitdh] = useState(0);
+    const [idLocation, setIdLocation] = useState(getRandomLocation());
+    const url = `https://rickandmortyapi.com/api/location/${idLocation}`;
+    const [location, error, isLoading, totalResidents] = useUrl(url);
+
+    const arrayPages = []
+    let pages = 1
+    if (totalResidents > perPages) {
+        pages = Math.ceil(totalResidents / perPages);
+        for (let i = 1; i <= pages; i++) {
+            arrayPages.push(i)
+        }
+    } 
 
     const handleSubmit = e => {
         e.preventDefault()
@@ -24,14 +36,23 @@ function App() {
     }
 
     const changeWidth = () => {
-        if ( witdh === 0) {
+        if (witdh === 0) {
             setWitdh("100%")
         } else {
             setWitdh(0)
         }
     }
 
-    console.log(witdh)
+    const changePage = e => {
+        if( perPages * e.target.innerText <= totalResidents) {
+            setInitialPage( perPages * e.target.innerText - perPages )
+            setLastpage( perPages * e.target.innerText ) 
+        } else {
+            setInitialPage(totalResidents - perPages)
+            setLastpage(totalResidents)
+        }
+    }
+
     return (
         <>
             {isLoading && <Loader />}
@@ -52,16 +73,26 @@ function App() {
                         <li><span>Type</span><span>{location?.type}</span></li>
                         <li><span>Dimension n°{idLocation}</span><span>{location?.dimension}</span></li>
                         <li><span>Residents</span><span>{location?.residents.length}</span></li>
-                        <div className='pagination' style={{width:`${witdh}`}}>
+                        <div className='pagination' style={{ width: `${witdh}` }}>
                             <h4>Pages</h4>
-                        </div>   
-                        <i className={ witdh === '100%' ? 'bx bxs-right-arrow' : 'bx bxs-left-arrow' } onClick={changeWidth} ></i>
+                            <div className='pages-container'>
+                                {
+                                    arrayPages.length === 0
+                                        ? <h4>This dimension only has one page</h4>
+                                        : arrayPages.map(page => (
+                                            <button key={page} className='page-button' onClick={changePage}> {page} </button>
+                                        ))
+
+                                }
+                            </div>
+                        </div>
+                        <i className={witdh === '100%' ? 'bx bxs-right-arrow' : 'bx bxs-left-arrow'} onClick={changeWidth} ></i>
                     </ul>
-                    
+
                 </article>
                 <main className='cards-container'>
                     {
-                        location?.residents.map(url => (
+                        location?.residents.slice(initialPage, lastpage).map(url => (
                             <ResidentCard key={url} url={url} />
                         ))
                     }
--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -6,19 +6,23 @@ const useUrl = (url) => {
     const [state, setState] = useState();
     const [error, setError] = useState(false);
     const [isLoading, setIsLoading] = useState(false); 
+    const [resLength, setResLength] = useState(0)
+
 
     useEffect(() => {
         setIsLoading(true)
         axios.get(url)
             .then(res => {
                 setState(res.data)
+                setResLength(res.data.residents.length)
                 setError(false)
             })
             .catch(() => setError(true))
             .finally(() => setIsLoading(false))
     }, [url])
 
-    return [state, error, isLoading]
+
+    return [ state, error, isLoading, resLength ]
 
 }
 
